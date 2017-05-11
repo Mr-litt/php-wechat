5fee83fd0b74f56083407c0f21fd7b35c189270b
lihaitao <lihaitao@qq.com>
2017-05-11 18:18:00 +0800

    base get and set

--- src/wechat/app/Core/Base.php
+++ src/wechat/app/Core/Base.php
@@ -1,13 +1,37 @@
 <?php
 
-namespace wechat\app\Core;
 /**
- * Created by IntelliJ IDEA.
- * User: lihaitao
- * Date: 17-4-28
- * Time: 下午3:23
+ * Base.php
+ *
+ * @author  Mr.litt<137057181@qq.com>
+ * @date    17-5-11
  */
+
+namespace wechat\app\Core;
+
+
 class Base
 {
+
+    public function __get($name)
+    {
+        $getter = 'get' . $name;
+        if (method_exists($this, $getter)) {
+            return $this->$getter();
+        } else {
+            throw new \Exception('getting unknown property');
+        }
+    }
+
+
+    public function __set($name, $value)
+    {
+        $setter = 'set' . $name;
+        if (method_exists($this, $setter)) {
+            $this->$setter($value);
+        } else {
+            throw new \Exception('setting unknown property');
+        }
+    }
 
 }
