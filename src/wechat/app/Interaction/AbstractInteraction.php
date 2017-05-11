ef478adb0c3a68a7d0bbbc7b1f19f59e83d42f45
Mr-litt <137057181@qq.com>
2017-05-11 18:16:17 +0800

    Adjust

--- src/wechat/app/Interaction/AbstractInteraction.php
+++ src/wechat/app/Interaction/AbstractInteraction.php
@@ -13,25 +13,5 @@ use wechat\app\Core\Collection;
 
 abstract class AbstractInteraction extends Collection
 {
-    protected static $to = "";
-    protected static $from = "";
 
-
-    protected function getTo(){
-        self::$to == "" && (new Request());
-        return self::$to;
-    }
-
-    protected function getFrom(){
-        self::$from == "" && (new Request());
-        return self::$from;
-    }
-
-    protected function setTo($to){
-        self::$to = $to;
-    }
-
-    protected function setFrom($from){
-        self::$from = $from;
-    }
 }
