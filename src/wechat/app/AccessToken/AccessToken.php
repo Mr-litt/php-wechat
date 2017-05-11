84f90725f7f5715ee486ba3cd45f92a338b2de87
lihaitao <lihaitao@qq.com>
2017-05-11 18:16:48 +0800

    accessToken

---
+++ src/wechat/app/AccessToken/AccessToken.php
@@ -0,0 +1,38 @@
+<?php
+namespace wechat\app\AccessToken;
+
+/**
+ * AccessToken.php
+ *
+ * @author  Mr.litt<137057181@qq.com>
+ * @date    17-5-11
+ */
+
+
+/**
+ * Class AccessToken
+ * @package wechat\app\AccessToken
+ *
+ */
+class AccessToken extends AbstractAccessToken
+{
+    const ACCESS_TOKEN_GET = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET';
+
+    public function get(){
+        $cache = $this->getCache(AccessToken::class);
+        if(empty($cache) || $cache['update_time']+$cache['expire_in'] <= time()){
+            $cache = $this->set();
+        }
+        return $cache['access_token'];
+    }
+
+    private function set(){
+        $cache = [];
+        $result = $this->http(self::ACCESS_TOKEN_GET);
+        $cache["update_time"] = time();
+        $cache["access_token"] = $result->access_token;
+        $cache["expires_in"] = $result->expires_in;
+        $this->setCache(AccessToken::class,$cache);
+        return $cache;
+    }
+}
---
+++ src/wechat/components/Curl.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Curl.php
+ *
+ * @author  Mr.litt<137057181@qq.com>
+ * @date    17-5-11
+ */
+
+namespace wechat\components;
+
+
+class Curl
+{
+    /**
+     * post
+     * @param $url
+     * @param array $postFields
+     * @param array $header
+     * @return mixed
+     */
+    public static function post($url,$postFields=[],$header =[]){
+        if(is_array($postFields)) $postFields = http_build_query($postFields);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+    /**
+     * get
+     * @param $url
+     * @param array $getFields
+     * @return mixed
+     */
+    public static function get($url,$getFields=[]){
+        $getFields = http_build_query($getFields);
+        $ch = curl_init();
+        $url .='?'.$getFields;
+        curl_setopt($ch, CURLOPT_POST, 0);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+}
