dcf2eafe20d09cdcde2bdc8e51379e9565ddcc21
Mr-litt <137057181@qq.com>
2017-05-11 18:16:57 +0800

    Menu

--- src/wechat/app/Menu/Menu.php
+++ src/wechat/app/Menu/Menu.php
@@ -1,6 +1,6 @@
 <?php
 namespace wechat\app\Menu;
-use wechat\app\Core\Base;
+use wechat\app\Core\Api;
 
 
 /**
@@ -9,8 +9,25 @@ use wechat\app\Core\Base;
  * Date: 17-5-2
  * Time: 下午6:07
  */
-class Menu extends Base
+class Menu extends Api
 {
 
+    const API_CREATE = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN';
+    const API_GET = 'https://api.weixin.qq.com/cgi-bin/menu/get?access_token=ACCESS_TOKEN';
+    const API_DELETE = 'https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN';
+
+    public function create($menu){
+        $menu = json_encode($menu);
+        $this->http(self::API_CREATE,"post",$menu);
+    }
+
+    public function get(){
+        return $this->http(self::API_GET);
+    }
+
+
+    public function delete(){
+        $this->http(self::API_DELETE);
+    }
 
 }
